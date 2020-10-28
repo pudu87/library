7f44541b248650a512f7d6df3fe6f7fe91ac1145
pudu87 <sam.cappelle87@gmail.com>
2020-10-28 17:38:40 -0300

    refactor script

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 let myLibrary = [];
 
+// QUERY'S
+
+const table = document.querySelector('table');
+const newBookBtn = document.querySelector('.new-book button');
+const newBookForm = document.querySelector('.new-book form');
+const newBookInput = document.querySelectorAll('.new-book input');
+
+// CONSTRUCTORS
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,61 +21,69 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readInfo}.`;
 };
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-  const table = document.querySelector('table');
-  const tr = document.createElement('tr');
-  tr.className = `book-${myLibrary.length-1}`;
-  console.log(Object.keys(book));
-  Object.keys(book).forEach(item => {
-    const td = document.createElement('td');
-    if (item == "read") {
-      td.textContent = book[item] === true ? "Yes" : "No";
-      const btn = document.createElement('button');
-      btn.textContent = "change";
-      td.appendChild(btn);
-    } else {
-      td.textContent = book[item];
-    }
-    tr.appendChild(td);
-  })
-  const td = document.createElement('td');
-  const btn = document.createElement('button');
-  btn.textContent = "X";
-  td.appendChild(btn);
-  tr.appendChild(td);
-  table.appendChild(tr);
-}
+// FUNCTIONS
 
-const newBookBtn = document.querySelector('.new-book button');
-newBookBtn.addEventListener('click', () => {
-  const newBookForm = document.querySelector('.new-book form');
-  newBookForm.hidden ? newBookForm.hidden = false : newBookForm.hidden = true;
-})
-
-let input = [];
 function submitForm() {
-  input = [];
-  let result;
-  result = document.querySelectorAll('.new-book input');
-  result.forEach(item => {
+  let input = [];
+  newBookInput.forEach(item => {
     if (item.type === "checkbox") {
       item.checked ? input.push(true) : input.push(false);
+    } else if (item.type === "number") {
+      input.push(parseInt(item.value));
     } else {
       input.push(item.value);
     }
-  })
+  });
   input.pop();
-  input[2] = parseInt(input[2]);
   let book = new Book(...input);
   addBookToLibrary(book);
   return false;
 }
 
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  let tr = document.createElement('tr');
+  tr.className = `book-${myLibrary.length-1}`;
+  Object.keys(book).forEach(item => {
+    let td = document.createElement('td');
+    if (item == "read") {
+      createTDRead(book, item, td);
+    } else {
+      td.textContent = book[item];
+    }
+    tr.appendChild(td);
+  });
+  createTDDelete(tr);
+  table.appendChild(tr);
+}
+
+function createTDRead(book, item, td) {
+  let span = document.createElement('span');
+  span.textContent = book[item] === true ? "Yes" : "No";
+  td.appendChild(span);
+  let btn = document.createElement('button');
+  btn.textContent = "change";
+  td.appendChild(btn);
+}
+
+function createTDDelete(tr) {
+  let td = document.createElement('td');
+  let btn = document.createElement('button');
+  btn.textContent = "\u00D7";
+  td.appendChild(btn);
+  tr.appendChild(td);
+}
+
+// EVENTS
+
+newBookBtn.addEventListener('click', () => {
+  newBookForm.hidden ? newBookForm.hidden = false : newBookForm.hidden = true;
+});
+
+// PREPOPULATE
+
 let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 309, true);
 addBookToLibrary(theHobbit);
-console.log(theHobbit)
-console.log(theHobbit.info());
 
 let theGodDelusion = new Book("The God Delusion", "Richard Dawkins", 463, false);
 addBookToLibrary(theGodDelusion);
